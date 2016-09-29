a5a81bcd469d1b9dbf3819a99f5f76b6db607aee
Gopichand Nadikatla <gopichandnadikatla@Suneels-MacBook-Pro-2.local>
2016-09-29 15:53:01 +0530

    Initial Commit

---
+++ DataPassingTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataPassingTable
+//
+//  Created by Gopichand Nadikatla on 9/29/16.
+//  Copyright © 2016 RedefinedGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DataPassingTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataPassingTable
+//
+//  Created by Gopichand Nadikatla on 9/29/16.
+//  Copyright © 2016 RedefinedGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
